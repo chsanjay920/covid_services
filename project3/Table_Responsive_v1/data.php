ed19043527a92e266ff141abda3da30107ab431f
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-05-13 16:09:33 +0530

    Add files via upload

---
+++ project3/Table_Responsive_v1/data.php
@@ -0,0 +1,19 @@
+<?php
+
+$user = "root"; //Enter the user name
+$password = ""; //Enter the password
+$host = "localhost"; //Enter the host
+$dbase = "covid_res"; //Enter the database
+$table = "res"; //Enter the table name
+
+
+$connection= mysqli_connect ($host, $user, $password,$dbase);
+if ($connection)
+{
+    // echo "connection sucess ";
+}
+else{
+    echo " Connection Failed".mysqli_connect_error();
+}
+
+?>
